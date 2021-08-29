9617277f1ceaa76c9eab2f255a8937f52171053a
Waleed-Abdullah <w26waleed@gmail.com>
2021-08-29 12:04:01 +0500

    Merge multi level DoublyLL

--- 9. Merge Multi-Level Doubly Linked List/Merge MultiLevel Doubly Linked List.js
+++ 9. Merge Multi-Level Doubly Linked List/Merge MultiLevel Doubly Linked List.js
@@ -44,3 +44,48 @@ var flatten = function (head) {
   }
   return head;
 };
+
+//2
+/**
+ * // Definition for a Node.
+ * function Node(val,prev,next,child) {
+ *    this.val = val;
+ *    this.prev = prev;
+ *    this.next = next;
+ *    this.child = child;
+ * };
+ */
+
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+var flatten = function (head) {
+  if (!head) return null;
+  if (!head.child && !head.next) return head;
+
+  let currentNode = head;
+
+  while (currentNode) {
+    if (currentNode.child) {
+      const child = currentNode.child;
+      const tail = currentNode.next;
+      currentNode.next = child;
+      child.prev = currentNode;
+      currentNode.child = null;
+
+      if (tail) {
+        let childTail = child;
+
+        while (childTail.next !== null) {
+          childTail = childTail.next;
+        }
+
+        childTail.next = tail;
+        tail.prev = childTail;
+      }
+    }
+    currentNode = currentNode.next;
+  }
+  return head;
+};
