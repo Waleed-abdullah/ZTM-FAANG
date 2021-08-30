16d73e3ca2ccbd62caa6d5cb0275531301ad91d8
Waleed-Abdullah <w26waleed@gmail.com>
2021-08-30 17:11:35 +0500

    floyds algo

--- 10. LL Cycle Detection/Floyd's algorithm.js
+++ 10. LL Cycle Detection/Floyd's algorithm.js
@@ -6,20 +6,20 @@ var detectCycle = function (head) {
   if (!head) return null;
 
   let hare = head;
-  let tort = head;
+  let tortoise = head;
 
   while (true) {
     hare = hare.next;
-    tort = tort.next;
+    tortoise = tortoise.next;
 
     if (hare === null || hare.next === null) return null;
     else hare = hare.next;
 
-    if (hare === tort) break;
+    if (hare === tortoise) break;
   }
 
   let p1 = head;
-  let p2 = tort;
+  let p2 = tortoise;
 
   while (p1 !== p2) {
     p1 = p1.next;
