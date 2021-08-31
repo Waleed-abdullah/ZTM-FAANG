1bbfe21b7716aef241a0148994c4f1d71c77700b
Waleed-Abdullah <w26waleed@gmail.com>
2021-08-31 10:56:37 +0500

    valid parenthises

---
+++ 11. Valid parenthesis Stacks and queues/Valid Parenthesis.js
@@ -0,0 +1,32 @@
+//Time O(n) Space O(1)
+var isValid = function (s) {
+  if (s.length === 0) return true;
+  if (s.length === 1) return false;
+
+  let stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === ')' || s[i] === '}' || s[i] === ']') {
+      if (stack.length === 0) return false;
+      else {
+        const leftBracket = stack.pop();
+        const rightBracket = s[i];
+        switch (leftBracket) {
+          case '(':
+            if (rightBracket !== ')') return false;
+            break;
+          case '[':
+            if (rightBracket !== ']') return false;
+            break;
+          case '{':
+            if (rightBracket !== '}') return false;
+            break;
+        }
+      }
+    } else {
+      stack.push(s[i]);
+    }
+  }
+  if (stack.length === 0) return true;
+  else return false;
+};
