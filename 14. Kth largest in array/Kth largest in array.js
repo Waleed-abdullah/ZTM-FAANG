e49aabb1adec040d1b5c3656aa8887d8ece73284
Waleed-Abdullah <w26waleed@gmail.com>
2021-09-02 14:25:17 +0500

    kth largest in array naive solution

---
+++ 14. Kth largest in array/Kth largest in array.js
@@ -0,0 +1,39 @@
+var findKthLargest = function (nums, k) {
+  // quickSort(nums, 0, nums.length -1);
+  nums.sort((a, b) => a - b);
+  return nums[nums.length - k];
+};
+
+function quickSort(array, left, right) {
+  let pivot;
+  let partitionIndex;
+
+  if (left < right) {
+    pivot = right;
+    partitionIndex = partition(array, pivot, left, right);
+
+    quickSort(array, left, partitionIndex - 1);
+    quickSort(array, partitionIndex + 1, right);
+  }
+  return array;
+}
+
+function partition(array, pivot, left, right) {
+  let pivotValue = array[pivot];
+  let partitionIndex = left;
+
+  for (let i = left; i < right; i++) {
+    if (array[i] < pivotValue) {
+      swap(array, i, partitionIndex);
+      partitionIndex++;
+    }
+  }
+  swap(array, right, partitionIndex);
+  return partitionIndex;
+}
+
+function swap(array, firstIndex, secondIndex) {
+  var temp = array[firstIndex];
+  array[firstIndex] = array[secondIndex];
+  array[secondIndex] = temp;
+}
