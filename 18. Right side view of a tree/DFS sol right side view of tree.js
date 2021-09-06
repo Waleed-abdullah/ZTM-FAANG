8c66b52bd1390595074278b7865854f74dc4eff5
Waleed-Abdullah <w26waleed@gmail.com>
2021-09-06 12:01:34 +0500

    DFS solution

--- 18. Right side view of a tree/DFS sol right side view of tree.js
+++ 18. Right side view of a tree/DFS sol right side view of tree.js
@@ -1,30 +1,19 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {number[]}
- */
- var rightSideView = function(root) {
-    if(!root) return [];
-        
-    const result = [];
-    DFS(root, 0, result)
-    return result;
+// Time : O(n) Space: O(n) worst case for recursive function O(h) where h is the height of the tree
+var rightSideView = function (root) {
+  if (!root) return [];
+
+  const result = [];
+  DFS(root, 0, result);
+  return result;
 };
 
-const DFS = (node, currentLevel = 0, result) =>{
-    if(!node) return;
-    
-    if(currentLevel >= result.length){
-        result.push(node.val);
-    }
-    
-    if(node.right) DFS(node.right, currentLevel + 1, result);
-    if(node.left) DFS(node.left, currentLevel + 1, result);
-}
+const DFS = (node, currentLevel = 0, result) => {
+  if (!node) return;
+
+  if (currentLevel >= result.length) {
+    result.push(node.val);
+  }
+
+  if (node.right) DFS(node.right, currentLevel + 1, result);
+  if (node.left) DFS(node.left, currentLevel + 1, result);
+};
