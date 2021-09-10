bf1321aa0b729ba80f0c78a31ddb68a659479350
Waleed-Abdullah <w26waleed@gmail.com>
2021-09-10 15:35:46 +0500

    validate BST

---
+++ 20. Validate BST/Validate BST.js
@@ -0,0 +1,16 @@
+// Time: O(n) Space: O(h)
+var isValidBST = function (node, low = null, high = null) {
+  if (!node) return true;
+  //check with the boundaries
+  if (
+    (high !== null && node.val >= high) ||
+    (low !== null && node.val <= low)
+  ) {
+    return false;
+  }
+  //validate right and left nodes of each subtree according to the boundries given
+  return (
+    isValidBST(node.left, low, node.val) &&
+    isValidBST(node.right, node.val, high)
+  );
+};
