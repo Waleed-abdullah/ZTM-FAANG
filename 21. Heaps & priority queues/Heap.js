b0a3f29aff6e7f66f7198eb6d4157f59854c4069
Waleed-Abdullah <w26waleed@gmail.com>
2021-09-11 12:11:14 +0500

    max and min heaps

---
+++ 21. Heaps & priority queues/Heap.js
@@ -0,0 +1,13 @@
+/*
+There are two types of heaps max and min heaps, they are complete binary trees.
+
+Max Heap: every node has children with values smaller than itself
+Min Heap: every node has children with values greater than itself
+
+Max and Min heaps are used to implement priority queues
+
+The space and time complexity is given as:
+Space: O(n)
+Insertion: O(log n)
+Deletion: O(log n)
+*/
