e4adbba45fb5678975723f7cdff62d0cb76591d5
Waleed-Abdullah <w26waleed@gmail.com>
2021-09-16 18:13:05 +0500

    rotting oranges solution

--- 24. 2D arrays Rotting oranges/2D arr Rotting oranges.js
+++ 24. 2D arrays Rotting oranges/2D arr Rotting oranges.js
@@ -1,7 +1,4 @@
-/**
- * @param {number[][]} grid
- * @return {number}
- */
+//Time complexity: O(n) Space complexity: O(n)
 
 const directions = [
   [1, 0], //up
@@ -14,46 +11,54 @@ const orangesRotting = function (grid) {
   if (grid.length === 0 || grid[0].length === 0) return 0;
 
   let minutes = 0;
-  let rottenOranges = 0;
+  let freshOranges = 0;
+  const rottenOranges = []; //queue for BFS
+
+  freshOranges = countOranges(grid, rottenOranges);
+
+  while (rottenOranges.length) {
+    const length = rottenOranges.length;
+    let count = 0;
+
+    for (let i = 0; i < length; i++) {
+      const [row, col] = rottenOranges.shift();
+
+      for (let j = 0; j < directions.length; j++) {
+        const currRow = row + directions[j][0];
+        const currCol = col + directions[j][1];
+
+        if (
+          currRow < 0 ||
+          currRow >= grid.length ||
+          currCol < 0 ||
+          currCol >= grid[currRow].length
+        )
+          continue;
+
+        if (grid[currRow][currCol] === 1) {
+          grid[currRow][currCol] = 2;
+          rottenOranges.push([currRow, currCol]);
+          freshOranges--;
+          count++;
+        }
+      }
+    }
+
+    if (count > 0) minutes++;
+  }
+
+  if (freshOranges > 0) return -1;
+  else return minutes;
+};
+
+const countOranges = function (grid, rottenOranges) {
+  let freshOranges = 0;
 
   for (let row = 0; row < grid.length; row++) {
     for (let col = 0; col < grid[row].length; col++) {
-      if (grid[row][col] === 2) {
-        rottenOranges++;
-        minutes += rot(grid, row, col);
-      }
-      if (rottenOranges > 0 && grid[row][col] === 1) {
-        return -1;
-      }
+      if (grid[row][col] === 2) rottenOranges.push([row, col]);
+      else if (grid[row][col] === 1) freshOranges++;
     }
   }
-
-  if (rottenOranges === 0) return 0;
-  return minutes;
-};
-
-const rot = function (grid, row, col) {
-  let count = 0;
-
-  for (let i = 0; i < directions.length; i++) {
-    const vertical = row + directions[i][0];
-    const horizontal = col + directions[i][1];
-
-    if (
-      vertical < 0 ||
-      vertical >= grid.length ||
-      horizontal < 0 ||
-      horizontal >= grid[row].length ||
-      grid[vertical][horizontal] === 0
-    )
-      continue;
-
-    if (grid[vertical][horizontal] === 1) {
-      grid[vertical][horizontal] = 2;
-      count++;
-    }
-  }
-
-  if (count > 0) return 1;
-  else return 0;
+  return freshOranges;
 };
